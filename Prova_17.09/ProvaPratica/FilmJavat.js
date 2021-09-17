9538a34a53137d5c1516040184bdec523a3f1d7c
eledalsav <87308712+eledalsav@users.noreply.github.com>
2021-09-17 15:36:02 +0200

    Add files via upload

---
+++ Prova_17.09/ProvaPratica/FilmJavat.js
@@ -0,0 +1,68 @@
+﻿function save() {
+    let insertedName = document.getElementById("name").value;
+
+    localStorage.setItem('name', insertedName)
+}
+
+function change() {
+    let inputs = document.querySelectorAll("input[type='name']");
+    let loginButton = document.querySelector("input[type=submit]");
+
+    for (let i = 0; i < inputs.length; i++) {
+        if (inputs[i].value == "") 
+        {
+            loginButton.disabled = true; 
+            return; 
+        }
+    }
+    loginButton.disabled = false; 
+}
+
+function hideForm() {
+    
+    let form = document.getElementById("formLogin");
+
+    form.style.display = "none"; 
+
+    
+    let unsubscribebutton = document.getElementById("unsubscribeButton");
+
+    unsubscribebutton.style.display = "";
+}
+
+function checkRegistredUser() {
+   
+    let storedName = localStorage.getItem('name'); 
+   
+    if (storedName != null ) 
+    {
+       
+        hideForm(); 
+    }
+
+}
+
+function unsubscribe() {
+
+    localStorage.clear();
+
+    let form = document.getElementById("formLogin");
+    form.style.display = "";
+
+    let subscribeFormButton = document.querySelector("input[type=submit]");
+    subscribeFormButton.disabled = false;
+
+
+    let unsubscribe2Button = document.getElementById("unsubscribeButton");
+    unsubscribe2Button.style.display = "none";
+
+}
+
+function checkwelcome() {
+    let storedName = localStorage.getItem('name');
+
+    if (storedName != null) {
+
+        alert("Benvenuto " + storedName);
+    }
+}
